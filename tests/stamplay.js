ee3f2a3dfaebebac596e43da499058658a84af4c
Claudio Petrini <claudio.petrini@stamplay.com>
2014-10-22 16:24:23 +0200

    Exposing webhook object

--- tests/stamplay.js
+++ tests/stamplay.js
@@ -2,17 +2,21 @@ suite('Stamplay Object ', function () {
 
 	test('exists as window property', function () {
 		assert.typeOf(window.Stamplay, 'object', 'window.Stamplay exists');
-	})
+	});
 
 	test('expose Support object', function () {
 		assert.typeOf(window.Stamplay.Support, 'object', 'window.Stamplay.Support exists');
-	})
+	});
 
 	test('expose Cobject constructor', function () {
 		assert.typeOf(window.Stamplay.Cobject, 'function', 'window.Stamplay.Cobject constructor exists');
-	})
+	});
 
 	test('expose User constructor', function () {
 		assert.typeOf(window.Stamplay.User, 'function', 'window.Stamplay.User constructor exists');
-	})
+	});
+
+	test('expose Webhook constructor', function () {
+		assert.typeOf(window.Stamplay.Webhook, 'function', 'window.Stamplay.Webhook constructor exists');
+	});
 })
