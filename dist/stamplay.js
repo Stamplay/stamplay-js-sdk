84b04a78c61be34f2871db31d2efa6adc842fab8
Cristian Roselli <cristian.roselli@stamplay.com>
2015-03-19 15:35:21 +0100

    Added sort on Query obj

--- dist/stamplay.js
+++ dist/stamplay.js
@@ -2870,6 +2870,24 @@ return Q;
 			return this
 		}
 
+		this.sortAscending = function(value){
+			var obj = {
+				$sort: {}
+			}
+			obj.$sort[value] = 1
+			this.currentQuery.push(obj)
+			return this
+		}
+
+		this.sortDescending = function(value){
+			var obj = {
+				$sort: {}
+			}
+			obj.$sort[value] = -1
+			this.currentQuery.push(obj)
+			return this
+		}
+
 		this.exists = function(attr){
 			var obj = {}
 			obj[attr] = {"$exists":true}
--- src/query.js
+++ src/query.js
@@ -59,6 +59,24 @@
 			return this
 		}
 
+		this.sortAscending = function(value){
+			var obj = {
+				$sort: {}
+			}
+			obj.$sort[value] = 1
+			this.currentQuery.push(obj)
+			return this
+		}
+
+		this.sortDescending = function(value){
+			var obj = {
+				$sort: {}
+			}
+			obj.$sort[value] = -1
+			this.currentQuery.push(obj)
+			return this
+		}
+
 		this.exists = function(attr){
 			var obj = {}
 			obj[attr] = {"$exists":true}
