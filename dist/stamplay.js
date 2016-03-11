4d7e419efdd5a466a68b3632eb47e0ac4224d135
Cristian Roselli <cristian.roselli@stamplay.com>
2016-03-11 09:27:51 +0100

    New dist version

--- dist/stamplay.js
+++ dist/stamplay.js
@@ -1,4 +1,4 @@
-/*! Stamplay v2.0.2 | (c) 2016 Stamplay *///     Underscore.js 1.8.3
+/*! Stamplay v2.0.3 | (c) 2016 Stamplay *///     Underscore.js 1.8.3
 //     http://underscorejs.org
 //     (c) 2009-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
 //     Underscore may be freely distributed under the MIT license.
